975603ce58976c8df6e56d3a146d93da32d78a80
jsyx <ly020922@aliyun.com>
2026-05-22 09:11:08 +0800

    [leetcode每日一题]

---
+++ leetcode/每日一题/2026/05/22.cpp
@@ -0,0 +1,29 @@
+//https://leetcode.cn/problems/search-in-rotated-sorted-array/description/?envType=daily-question&envId=2026-05-22
+
+
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int n = nums.size();
+        int l = 0,r = n - 1;
+        while(l <= r){
+            int mid = l + r >> 1;
+            if(nums[mid] == target)return mid;
+            if(nums[0] <= nums[mid]){
+                if(nums[0] <= target && target < nums[mid]){
+                    r = mid - 1;
+                }else {
+                    l = mid + 1;
+                }
+            }else {
+                if(nums[mid] < target && target <= nums[n - 1]){
+                    l = mid + 1;
+                }else {
+                    r = mid - 1;
+                }
+            }
+
+        }
+        return -1;
+    }
+};
