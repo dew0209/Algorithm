6869c3f2232fde71c2760a8cd8d64beb1dc09d4a
jsyx <ly020922@aliyun.com>
2026-08-10 16:45:23 +0800

    [leetcode 每日一题]

---
+++ leetcode/每日一题/2026/08/09.cpp
@@ -0,0 +1,21 @@
+//https://leetcode.cn/problems/stone-game-ii/?envType=daily-question&envId=2026-08-09
+
+class Solution {
+    public int stoneGameII(int[] piles) {
+    int len = piles.length, sum = 0;
+    int[][] dp = new int[len][len + 1];
+    for (int i = len - 1; i >= 0; i--) {
+        sum += piles[i];
+        for (int M = 1; M <= len; M++) {
+            if (i + 2 * M >= len) {
+                dp[i][M] = sum;
+            } else {
+                for (int x = 1; x <= 2 * M; x++) {
+                    dp[i][M] = Math.max(dp[i][M], sum - dp[i + x][Math.max(M, x)]);
+                }
+            }
+        }
+    }
+    return dp[0][1];
+}
+}
